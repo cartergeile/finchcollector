0fe8814bd1f98de4d8c0c2a149e98d3d032258f6
Carter Geile <cartergeile@l.local>
2023-03-02 14:50:48 -0600

    adds finches and html pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+
+# lists of dummy finches
+finches = [
+  {'name': 'Lolo', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
+
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+# about route
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
