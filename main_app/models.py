eb3b70d04eea28524525f18980148937120e0e9d
Carter Geile <cartergeile@l.local>
2023-03-06 18:38:02 -0600

    successfully can create a finch

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=150)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 # lists of dummy finches
-finches = [
-  {'name': 'Lolo', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'description': 'gentle and loving', 'age': 2},
-]
+#
+  #{'name': 'Lolo', 'description': 'furry little demon', 'age': 3},
+  #{'name': 'Sachi', 'description': 'gentle and loving', 'age': 2},
+#]
 
 # Create your views here.
 
@@ -18,7 +19,23 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  # We pass data to a template very much like we did in Express!
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
   })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
